563c3b6e75dad3085a369a6cf19020f6f74b36c3
Kirk Matt <kirkmatt@proton.me>
2025-11-25 10:23:47 +0000

    updated flakes

--- flake.nix
+++ flake.nix
@@ -1,21 +1,32 @@
+# flake.nix
 {
-    description = "Flake for smlp systems";
+  description = "DevShell for a Meson + Boost project";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    outputs = { self, nixpkgs }:
-    let
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
-    in
-    {
-        devShells."x86_64-linux".default = pkgs.mkShell {
-            buildInputs = with pkgs; [ gmp meson ninja ];
-            installPhase = ''
-                meson setup -Dkay-prefix=$HOME/kay --prefix $HOME/.local build
-                ninja -C build install
-                '';
-        };
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in pkgs.mkShell {
+      nativeBuildInputs = [
+        pkgs.pkg-config
+      pkgs.meson
+        pkgs.ninja
+        pkgs.gmp
+      pkgs.cmake          # optional – some Boost parts use CMake detection
+        pkgs.python312        # optional – if you have a Python helper script
+      ];
+
+      buildInputs = [
+        pkgs.boost
+      ];
+
+      shellHook = ''
+        # export BOOST_ROOT=${pkgs.python312Packages.boost}
+        # export BOOST_INCLUDEDIR=${pkgs.python312Packages.boost}/include
+        # export BOOST_LIBRARYDIR=${pkgs.python312Packages.boost}/lib
+        echo "✅  DevShell ready – Meson ${pkgs.meson.version}, Boost ${pkgs.boost.dev.version}"
+      '';
     };
+  };
 }
