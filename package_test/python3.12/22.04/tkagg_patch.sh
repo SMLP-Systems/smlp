dabd0e492cb254065a1edaa975f3e08ecaec78d0
Dmitry Messerman <dmitry.messerman@gmail.com>
2026-03-09 15:52:56 +0200

    Added wheel test for ubuntu:22.04/python3.12

--- package_test/python3.12/22.04/tkagg_patch.sh
+++ package_test/python3.12/22.04/tkagg_patch.sh
@@ -3,4 +3,4 @@ sed -i.bak '/^from/iif os.path.exists("\/.dockerenv"): \
     print("Script is running inside a Docker container.") \
     import matplotlib \
     matplotlib.use("TkAgg") \
-    import matplotlib.pyplot as plt\n' /usr/local/lib/python3.13/dist-packages/smlp/run_smlp.py
+    import matplotlib.pyplot as plt\n' /usr/local/lib/python3.12/dist-packages/smlp/run_smlp.py
