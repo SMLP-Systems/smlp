091af41c80a766b7ebc15a9a5958f27bca593d99
zurabksmlp <you@example.com>
2026-05-09 13:58:58 +0300

    removed redundant assertion from smlp_subgroups.py

--- src/smlp_py/smlp_subgroups.py
+++ src/smlp_py/smlp_subgroups.py
@@ -769,7 +769,6 @@ class SubgroupDiscovery:
     # function to visualize the selected ranges).
     def _smlp_subgroups_single_response(self, feat_df:pd.DataFrame, resp_df:pd.DataFrame, 
             resp_name:str, pos_value:int, qf:str, dim:int, top_n:int):
-        assert pos_value == 0 or pos_value == 1
         cls_reg_mode = get_response_type(resp_df, resp_name)
         assert cls_reg_mode == CLASSIFICATION or cls_reg_mode == REGRESSION
         if cls_reg_mode == REGRESSION:
