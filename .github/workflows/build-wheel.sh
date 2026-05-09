e2aeb5109a40dd07c18c505a7753871f43ff004a
Franz Brauße <fbrausse@paxle.org>
2026-05-09 10:44:00 +0200

    ci: install gcc-toolset-13 on manylinux_2_28

--- .github/workflows/build-wheel.sh
+++ .github/workflows/build-wheel.sh
@@ -6,10 +6,16 @@ loc=$(dirname "$0")
 project=$1
 ghos=$2
 
-dnf -y install wget
+dnf -y install \
+	wget \
+	gcc-toolset-13
+
 . /opt/rh/gcc-toolset-13/enable
+
 source $loc/install-gmp.sh install
+
 source $loc/install-z3.sh install
+
 export GMP_ROOT=$gmp_R
 export Z3_PREFIX=$z3_R
 export LD_LIBRARY_PATH=$z3_R/lib:$gmp_R/lib
