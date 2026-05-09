58abfebc1357bdf3f7968cebc1906e5df67a3a2b
Franz Brauße <fbrausse@paxle.org>
2026-05-09 11:56:18 +0200

    ci: install to /tmp/prefix + use that for setup.py's envvars

--- .github/workflows/build-wheel.sh
+++ .github/workflows/build-wheel.sh
@@ -4,18 +4,30 @@ set -e -x
 
 loc=$(dirname "${BASH_SOURCE[0]}")
 project=$1
-ghos=$2
+os=$2
+tgt=$3
 
-dnf -y install \
-	wget \
-	gcc-toolset-13
+case "$os" in
+ubuntu-*)
+	dnf -y install \
+		wget \
+		gcc-toolset-13
 
-. /opt/rh/gcc-toolset-13/enable
+	. /opt/rh/gcc-toolset-13/enable
 
-source $loc/install-gmp.sh install
+	export R=$tgt
 
-source $loc/install-z3.sh install
+	source $loc/install-gmp.sh install
 
-export GMP_ROOT=$gmp_R
-export Z3_PREFIX=$z3_R
-export LD_LIBRARY_PATH=$z3_R/lib:$gmp_R/lib
+	source $loc/install-z3.sh install
+
+	export GMP_ROOT=$gmp_R
+	export Z3_PREFIX=$z3_R
+	export LD_LIBRARY_PATH=$z3_R/lib:$gmp_R/lib
+
+	;;
+*)
+	echo "error: OS unknown: $os" >&2
+	exit 1
+	;;
+esac
