eccf86cb18b10fd44e3df9c36adf2e4dac1353b2
Franz Brauße <fbrausse@paxle.org>
2026-05-11 10:25:29 +0200

    ci: sanitize comments + also set arch on macos

--- .github/workflows/install-deps.sh
+++ .github/workflows/install-deps.sh
@@ -4,11 +4,13 @@ set -e -x
 
 loc=$(dirname "${BASH_SOURCE[0]}")	# directory of this script
 project=$1				# project source directory
-os=$2					# Github runner OS identifier
+label=$2				# Github runner label
 tgt=$3					# prefix to install packages to
 
-case "$os" in
+case "$label" in
 ubuntu-*)
+	# This is executed inside a manylinux Docker image. In this case,
+	# manylinux_2_28 based on almalinux-8.10.
 	dnf -y install \
 		wget \
 		gcc-toolset-13
@@ -27,7 +29,7 @@ macos-26*)
 	export CC=gcc-13
 	export CXX=g++-13
 	export R=$tgt
-	#export HOST=aarch64-apple-darwin25.3.0
+	export HOST=$(uname -m)-apple-darwin25.3.0
 
 	source $loc/install-gmp.sh install
 
